d27933031787a5186a8d0eaeb1c153b03e8e5476
mike32n <mihaly32horvath@gmail.com>
2020-01-16 11:29:36 +0100

    display_board simplified

--- ui.py
+++ ui.py
@@ -1,25 +1,6 @@
 def display_board(board):
-    '''
-    Displays complete game board on the screen
-
-    Returns:
-    Nothing
-    '''
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if i == 0 or i == len(board) - 1:
-                board[i][j] = '#'
-            elif j == 0:
-                board[i][j] = '#'
-            elif j == len(board[i]) - 1:
-                if i == 16:  # This is the place of the gate
-                    board[i][j] = ' '  # This is the icon of the gate
-                else:
-                    board[i][j] = '#'
-            else:
-                board[i][j] = board[i][j]
-        print(*board[i])
-
+    for row in board:
+        print(*row)
 
 def display_player_stats(player):
     print(player["name"] + "   " + "Race: " + player["race"] + "   Health: " + str(player["health"]))
