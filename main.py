b59832f2855ce8d265079c4730414773aa2283f8
KriszProg <krisz.codecool@gmail.com>
2020-01-27 11:38:48 +0100

    changed weapon to sword and add new properties

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ PLAYER_ICON = '@'
 PLAYER_START_X = 3
 PLAYER_START_Y = 3
 
-WEAPON_ICON = 'W'
-WEAPON_START_X = 10
-WEAPON_START_Y = 10
+SWORD_ICON = 'S'
+SWORD_START_X = 10
+SWORD_START_Y = 10
 
-MONSTER_ICON = '§'
+MONSTER_ICON = 'Đ'
 MONSTER_START_X = 25
 MONSTER_START_Y = 2
 
@@ -29,26 +29,26 @@ def create_player():
     dictionary
     '''
     player_inv = {}
-    player = {'icon': PLAYER_ICON, "coord_x" : PLAYER_START_X, "coord_y": PLAYER_START_Y,"name" : "Lord-el-Melloi", "race" : "Human", "health" : 100}
+    player = {"icon": PLAYER_ICON, "name": "Lord-el-Melloi", "race": "Human", "health": 100, "coord_y": PLAYER_START_Y, "coord_x" : PLAYER_START_X}
     return player, player_inv
 
 
-def create_weapon():
-    weapon = {'icon': WEAPON_ICON, "coord_x" : WEAPON_START_X, "coord_y": WEAPON_START_Y, "name": "Sword", "type" : "collectible"}
-    return weapon
+def create_sword():
+    sword = {"type": "collectible", "name": "Sword", "icon": SWORD_ICON, "health": 50, "hitting_power": 50, "coord_y": SWORD_START_Y, "coord_x": SWORD_START_X}
+    return sword
 
 
 def create_monster():
-    monster = {'icon': MONSTER_ICON, "coord_x" : MONSTER_START_X, "coord_y": MONSTER_START_Y}
+    monster = {"type": "enemy", "name": "Monster", "icon": MONSTER_ICON, "health": 150, "hitting_power": 100, "coord_y": MONSTER_START_Y, "coord_x": MONSTER_START_X}
     return monster
 
 
 def main():
     player, player_inv = create_player()
-    weapon = create_weapon()
+    sword = create_sword()
     monster = create_monster()
 
-    board1 = engine.create_board(gate1=(16, 29), gate2=(16, 0))    
+    board1 = engine.create_board(gate1=(16, 29), gate2=(16, 0))
 
     util.clear_screen()
 
@@ -58,7 +58,7 @@ def main():
 
     is_running = True
     while is_running:
-        engine.put_item_on_board(board1, weapon)
+        engine.put_item_on_board(board1, sword)
         engine.put_item_on_board(board1, monster)
         engine.put_item_on_board(board1, player)
         ui.display_board(board1)
@@ -74,7 +74,7 @@ def main():
         else:
             engine.refresh_player_coord(key, player, board1)
             board1 = engine.create_board(gate1=(16, 29), gate2=(16, 0))
-            player_inv, weapon = engine.check_collectible(player, player_inv, weapon)
+            player_inv, weapon = engine.check_collectible(player, player_inv, sword)
         util.clear_screen()
 
 
