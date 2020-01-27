ca72ecffc22c1ff633ea65fbaed89905d160fef6
KriszProg <krisz.codecool@gmail.com>
2020-01-27 18:44:13 +0100

    new functions: collect_item() and put_everyone_on_board()

--- engine.py
+++ engine.py
@@ -10,7 +10,7 @@ def create_board(gate1, gate2=None, height=20, width=30, ):
         row = []
         for j in range(width):
             if i == 0 or i == height - 1 or j == 0 or j == width - 1:
-                row.append(wall)            
+                row.append(wall)
             else:
                 row.append(space)
         board.append(row)
@@ -19,16 +19,14 @@ def create_board(gate1, gate2=None, height=20, width=30, ):
     return board
 
 
-def put_item_on_board(board, item):
-    try:
-        row = item.get('coord_y')
-        col = item.get('coord_x')
-        board[row][col] = item['icon']
-    except:
-        pass
+def put_everyone_on_board(board, coordinates_who_is_alive):
+    for element in coordinates_who_is_alive:
+        row, col = coordinates_who_is_alive[element]
+        board[row][col] = element
+    return board
 
 
-def refresh_player_coord(key, player, board):
+def refresh_player_coord(key, player, player_inv, board, everyone_in_room1):
     row = player.get('coord_y')
     col = player.get('coord_x')
     if key == 'w':
@@ -44,11 +42,42 @@ def refresh_player_coord(key, player, board):
     else:
         player['coord_y'] = row
         player['coord_x'] = col
-        return player
+        searched_icon = board[row][col]
+        if check_what_is_it(searched_icon, everyone_in_room1) == 'item':
+            name_of_item = refresh_player_inventory(searched_icon, player_inv, everyone_in_room1)
+            collect_item(searched_icon, player, everyone_in_room1)
+            return player
 
 
 def check_if_wall(board, row, col):
     return board[row][col] == '#'
+
+
+def check_what_is_it(searched_icon, everyone_in_room1):
+    for element in everyone_in_room1:
+        if element['icon'] == searched_icon:
+            return element['type']
+
+
+def refresh_player_inventory(searched_icon, player_inv, everyone_in_room1):
+    # If this would returns the name (name of dictionary), then the
+    # collect item function would use this name as arg
+    for name_of_item in everyone_in_room1:
+        if name_of_item['icon'] == searched_icon:
+            if name_of_item['name'] not in player_inv.keys():
+                player_inv[name_of_item['name']] = {'name': name_of_item['name'], 'quantity': 1, 'hitting_power': name_of_item['hitting_power']}
+            else:
+                player_inv[name_of_item['name']]['quantity'] += 1
+            print('name_of_item:', name_of_item)
+            return player_inv
+
+
+def collect_item(searched_icon, player, everyone_in_room1):
+    for name in everyone_in_room1:
+        if name['icon'] == searched_icon:
+            player['health'] += name['health']
+            name['health'] = 0
+            return name
 
 
 def get_player_stats(player):
@@ -70,19 +99,3 @@ def get_player_stats(player):
         player["race"] = "Beastman"
         player["health"] = 150
     return player
-
-
-def check_collectible(player, player_inv, sword):
-    player_coord_x = player["coord_x"]
-    player_coord_y = player["coord_y"]
-    sword_coord_x = sword["coord_x"]
-    sword_coord_y = sword["coord_y"]
-    if player_coord_x == sword_coord_x and player_coord_y == sword_coord_y:
-        if sword["name"] not in player_inv.keys():
-            player_inv[sword["name"]] = 1
-        else:
-            player_inv[sword["name"]] += 1
-        sword["icon"] = "."
-        sword["coord_y"] = None
-        sword["coord_x"] = None
-    return player_inv, sword
